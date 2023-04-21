f08ddf3f7a619cec45930daf98cbd2fa4a67eacb
jsaique <jsaique11@gmail.com>
2023-04-20 23:17:38 -0400

    Hero image lines animation

--- components/header.tsx
+++ components/header.tsx
@@ -31,7 +31,7 @@ export const Header = () => {
   }, [setHamburgerMenuIsOpen]);
 
   return (
-    <header className="fixed top-0 left-0 w-full border-b border-transparent-white backdrop-blur-[12px]">
+    <header className="fixed top-0 left-0 z-10 w-full border-b border-transparent-white backdrop-blur-[12px]">
       <Container className="flex h-navigation-height">
         <Link className="flex items-center" href="/">
           <Logo />
