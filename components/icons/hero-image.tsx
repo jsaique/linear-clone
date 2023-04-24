c6495c4ac26527f1e475641ee826188e274a7451
jsaique <jsaique11@gmail.com>
2023-04-23 21:57:41 -0400

    Hero animating image lines

--- components/icons/hero-image.tsx
+++ components/icons/hero-image.tsx
@@ -2,11 +2,17 @@
 
 import classNames from "classnames";
 import { before } from "node:test";
+import { CSSProperties, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { transform } from "typescript";
 
 export const HeroImage = () => {
   const { ref, inView } = useInView({ threshold: 0.4, triggerOnce: true });
+  const [lines, setLines] = useState([
+    { direction: "to bottom", duration: 2000, size: 20 },
+    { direction: "to right", duration: 3000, size: 15 },
+  ]);
+
   return (
     // Wraping div to add perspective
     <div ref={ref} className="mt-[12.8rem] [perspective:2000px]">
@@ -18,11 +24,32 @@ export const HeroImage = () => {
           inView && "before:animate-image-glow"
         )}
       >
+        <div className="absolute top-0 left-0 z-20 h-full w-full">
+          {lines.map((line) => (
+            <span
+              style={
+                {
+                  "--direction": line.direction,
+                  "--size": line.size,
+                  "--animation-duration": `${line.duration}ms`,
+                } as CSSProperties
+              }
+              className={classNames(
+                "bg-glow-lines block absolute top-0 w-[10rem] h-[1px]",
+                line.direction === "to right" &&
+                  "left-0 h-[1px] w-[calc(var(--size)*1rem)] animate-glow-line-horizontal",
+                line.direction === "to bottom" &&
+                  "right-0 h-[calc(var(--size)*1rem)] w-[1px] animate-glow-line-vertical"
+              )}
+            />
+          ))}
+        </div>
         <svg
           width="100%"
           viewBox="0 0 1499 778"
           fill="none"
           className={classNames(
+            "absolute left-0 top-0 h-full w-full",
             "[&_path]:stroke-white [&_path]:[stroke-opacity:0.2] [&_path]:[stroke-dasharray:1] [&_path]:[stroke-dashoffset:1]",
             inView && "[&_path]:animate-sketch-lines"
           )}
